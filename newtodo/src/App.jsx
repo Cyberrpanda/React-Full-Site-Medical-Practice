a481b5577bfe9daa89a92d3f25607c82bd53ae81
Balogun Ithiel <ithielb9@gmail.com>
2024-01-31 15:44:02 +0100

    resources and pages

---
+++ newtodo/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect} from 'react'
+import "./index.css"
+
+
+export default function maincontent(){
+
+    return(
+        <>
+
+        
+        <div className='head'>
+        <nav className='menubar'>
+          <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Service</a></li>
+          <li><a href="">Pages</a></li>
+          <li><a href="">Contact</a></li>
+          </ul>
+        </nav>
+ 
+        <button className='button-appoint'>Appointment</button>
+        
+        </div>
+       
+       
+        
+        
+        
+        </>
+    )
+
+
+
+}
