c6e3829b2b6ccb0662a50703347259894c1951b6
Balogun Ithiel <ithielb9@gmail.com>
2024-05-23 18:09:58 +0100

    fixed implementation issues with home component

--- reactsite1/src/Appointment.jsx
+++ reactsite1/src/Appointment.jsx
@@ -1,35 +1,12 @@
 import "./index.css"
 import Footer from "./footer";
+import Navbar from "./Navbar";
 import "./assets/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css"
 export default function Appoint () {
    return(
    <>
   
-      <div className="head">
-       <>
-        <img className="logo" src="../img/Gemini_Generated_Image__1_-removebg-preview.png" />
-       </>
-       
-        <nav className="menubar">
-          <ul>
-            <li >
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Service</a>
-            </li>
-            <li>
-              <a href="">Pages</a>
-            </li>
-            <li>
-              <a href="">Contact</a>
-            </li>
-          </ul>
-        </nav>       
-      </div>
+      <Navbar></Navbar>
 
       <br />
       <br />
